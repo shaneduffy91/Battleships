f37a0d28d1a2103b34508352ba709ab8fd2eda18
shaneduffy91 <shaneduffy38@yahoo.ie>
2024-01-07 17:09:38 +0000

    first two functions for battleships game

--- run.py
+++ run.py
@@ -10,3 +10,21 @@ the number of ships, the player's name and the type of board
 (ie. player board or computer). It has a method for adding the
 grid and ships, in addition to printing the battleship board.
 """
+
+def _init_self(self, size, num_ships, name, type):
+    self.size = size
+    self.board = [["." for x in range(size)] for y in range(size)]
+    self.num_ships = num_ships
+    self.name = name
+    self.type = type
+    self.guesses = []
+    self.ships = []
+
+
+def print(self):
+    for row in self.board:
+        print(" ".join(row))
+
+def guesses(self, x, y):
+    self.guesses.append((x, y))
+    self.board[x] [y] = "x"
